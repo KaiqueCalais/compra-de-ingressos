e86f2d432b85cc51c68a43b0cbef8045c3a7ff03
Kaique Gonçalves <kaique121106@gmail.com>
2023-12-27 20:14:49 -0300

    Correção de bug Corrigido um bug onde a quantidade de ingressos a ser comprada podia ser igual ou menor a 0.

--- js/app.js
+++ js/app.js
@@ -4,11 +4,13 @@ function comprar() {
     let quantidadeDisponivel = parseInt(document.querySelector(`#qtd-${ingresso}`).textContent);
     const quantidadeDisponivelTexto = document.querySelector(`#qtd-${ingresso}`);
 
-    if (quantidade <= quantidadeDisponivel) {
+    if (quantidade <= quantidadeDisponivel && quantidade > 0) {
         quantidadeDisponivel -= quantidade;
 
         quantidadeDisponivelTexto.innerHTML = quantidadeDisponivel;
-    } else {
+    } else if (quantidade > quantidadeDisponivel) {
         alert(`Quantidade inválida. A quantidade disponível para ${ingresso} é de ${quantidadeDisponivel}.`);
+    } else {
+        alert('A quantidade não pode ser menor do que 1.')
     }
 }
